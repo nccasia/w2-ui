42a2d4b863c390e956325145734f046f5522b9d4
Tín Đoàn <tindoan9@gmail.com>
2022-12-22 20:21:24 +0700

    schema form create task

--- src/tasks/components/ChannelPickerDialog/TaskTypePickerDialog.stories.tsx
+++ src/tasks/components/ChannelPickerDialog/TaskTypePickerDialog.stories.tsx
@@ -1,6 +1,4 @@
-import { channelsList } from "@saleor/channels/fixtures";
 import Decorator from "@saleor/storybook/Decorator";
-import { mapNodeToChoice } from "@saleor/utils/maps";
 import { storiesOf } from "@storybook/react";
 import React from "react";
 
@@ -8,14 +6,10 @@ import TaskTypePickerDialog, {
   TaskTypePickerDialogProps,
 } from "./TaskTypePickerDialog";
 
-const channelsChoices = mapNodeToChoice(channelsList);
-
 const props: TaskTypePickerDialogProps = {
-  channelsChoices,
-  confirmButtonState: "default",
   onClose: () => undefined,
-  onConfirm: () => undefined,
   open: true,
+  TypeChoices: [],
 };
 
 storiesOf("Views / Type / Settings dialog", module)
