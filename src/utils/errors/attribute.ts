d8d5a9e02f86ad38954fa84a553ac0ee19bc8904
Tín Đoàn <tindoan9@gmail.com>
2023-01-03 14:15:30 +0700

    fix code home page, login

--- src/utils/errors/attribute.ts
+++ src/utils/errors/attribute.ts
@@ -1,27 +1,23 @@
-import { defineMessages, IntlShape } from "react-intl";
+import { IntlShape } from "react-intl";
 
 import { getCommonFormFieldErrorMessage } from "./common";
 
-const messages = defineMessages({
-  alreadyExists: {
-    id: "KFv8hX",
-    defaultMessage: "An attribute already exists.",
-  },
-  nameAlreadyTaken: {
-    id: "FuAV5G",
-    defaultMessage: "This name is already taken. Please provide another.",
-  },
-  notFound: {
-    id: "SKFr04",
-    defaultMessage: "Attribute not found.",
-  },
-});
+// const messages = defineMessages({
+//   alreadyExists: {
+//     id: "KFv8hX",
+//     defaultMessage: "An attribute already exists.",
+//   },
+//   nameAlreadyTaken: {
+//     id: "FuAV5G",
+//     defaultMessage: "This name is already taken. Please provide another.",
+//   },
+//   notFound: {
+//     id: "SKFr04",
+//     defaultMessage: "Attribute not found.",
+//   },
+// });
 
-function getAttributeErrorMessage(
-  err: any
-  intl: IntlShape,
-): string {
-
+function getAttributeErrorMessage(err: any, intl: IntlShape): string {
   return getCommonFormFieldErrorMessage(err, intl);
 }
 
