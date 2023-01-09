2b43281c2e611ac4db69933ceb182fedf014ab3f
Tín Đoàn <tindoan9@gmail.com>
2023-01-09 20:05:51 +0700

    call api create task

--- src/hooks/useChoiceSearch.ts
+++ src/hooks/useChoiceSearch.ts
@@ -2,7 +2,7 @@ import { Choice } from "@saleor/components/SingleSelectField";
 import { filter } from "fuzzaldrin";
 import { useMemo, useState } from "react";
 
-function useChoiceSearch(choices: Array<Choice<string | number, string>>) {
+function useChoiceSearch(choices: Array<Choice<string, string>>) {
   const [query, setQuery] = useState("");
 
   const sortedChoices = useMemo(
