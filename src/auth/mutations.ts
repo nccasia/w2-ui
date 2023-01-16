ecc8e5d486f293a183d8ddada73109d2f2209901
Tín Đoàn <tindoan9@gmail.com>
2023-01-16 13:32:07 +0700

    update

--- src/auth/mutations.ts
+++ src/auth/mutations.ts
@@ -1,11 +1,5 @@
 import { gql } from "@apollo/client";
 
-export const requestPasswordReset = gql`
-  query MyQuery {
-    hello(name: "world")
-  }
-`;
-
 export const login = gql`
   mutation Login($email: String = "", $password: String = "") {
     login(data: { email: $email, password: $password }) {
