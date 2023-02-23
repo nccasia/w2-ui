d3bca8d9aa2d5655eb90c9311028ae6d585759bc
VinhHuuTran <tranhuuvinh1109@gmail.com>
2023-02-23 09:07:23 +0700

    fix get and view my tasks

--- src/taskboard/views/TaskList/TaskList.tsx
+++ src/taskboard/views/TaskList/TaskList.tsx
@@ -15,7 +15,7 @@ import {
 } from "@saleor/taskboard/urls";
 import createDialogActionHandlers from "@saleor/utils/handlers/dialogActionHandlers";
 import { mapEdgesToItems } from "@saleor/utils/maps";
-import React, { useEffect, useMemo } from "react";
+import React, { useEffect } from "react";
 
 interface TaskListProps {
   params: TaskListUrlQueryParams;
@@ -35,53 +35,29 @@ export const TaskList: React.FC<TaskListProps> = ({
     channel: undefined,
   };
   const dataTaskBoard = useTaskBoard(id);
-  // const { data } = useGetTasksQuery({
-  //   variables,
-  // });
 
-  const newVariables = useMemo(() => {
-    if (user.user.userId) {
-      return {
-        ...variables,
-        id: user.user.userId,
-      };
-    }
-  }, [user.user?.userId]);
   const [fetchMyTask, { data }] = useGetMyTasksLazyQuery();
 
   const paginationValues = usePaginator({
     pageInfo: data?.Task_connection.pageInfo,
-    paginationState: newVariables,
+    paginationState: variables,
     queryString: {},
   });
-  // eslint-disable-next-line no-console
-  // console.log('fethMyTask', myTask)
-  // console.log(
-  //   "\x1b[44m%s\x1b[0m",
-  //   "TaskList.tsx line:41 paginationValues",
-  //   paginationValues, 'variable', {
-  //   ...variables,
-  //   _eq: user.user?.userId
-  // },
-  //   'userr', user,
-  //   "id",
-  //   id, myTask
-  // );
-  // eslint-disable-next-line no-console
-  // console.log("data", data?.Task_connection.edges, "vari", variables);
 
   const noTaskType = !channel && typeof channel !== "undefined";
   const [openModal, closeModal] = createDialogActionHandlers<
     TaskListUrlDialog,
     TaskListUrlQueryParams
   >(navigate, modalNewTaskUrl, params, id);
-  // eslint-disable-next-line no-console
-  // console.log("123", mapEdgesToItems(data?.Task_connection));
 
   useEffect(() => {
     if (user.user.userId) {
+      const temp = {
+        ...variables,
+        id: user.user.userId,
+      };
       fetchMyTask({
-        variables: newVariables,
+        variables: temp,
       });
     }
   }, [variables]);
